89a760b9f78586e5fd4dc0ddb0608d3ce24372d6
David Bickel <aethertier@mailueberfall.de>
2024-08-26 13:21:06 +0000

    Add substructure heuristics as data

--- Code/IDLPPBopt/config.py
+++ Code/IDLPPBopt/config.py
@@ -6,3 +6,6 @@ PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Path to the default model
 DEFAULT_MODEL_PATH = os.path.join(PACKAGE_ROOT, 'saved_models', 'model_ppb_3922_Tue_Dec_22_22-23-22_2020_54.pt')
+
+# Path to substructure heuristics
+GPSUBS_PATH = os.path.join(PACKAGE_ROOT, 'saved_models', 'GPSubs.csv')
