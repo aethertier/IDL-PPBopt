0cefbbaddd6f09bcfa529577d87091e4c8a62830
David Bickel <aethertier@mailueberfall.de>
2024-08-26 13:10:00 +0000

    Visualizer with rotating substructure colors

--- Code/IDLPPBopt/__init__.py
+++ Code/IDLPPBopt/__init__.py
@@ -1,3 +1,4 @@
 from .interface.PPBPredictor import PPBPredictor, PPBPredictorViz
+from .interface.Visualizer import AtomAttentionVisualizer
 
 __version__ = '0.1.2a1'
--- Code/IDLPPBopt/interface/Visualizer.py
+++ Code/IDLPPBopt/interface/Visualizer.py
@@ -1,8 +1,7 @@
-from typing import List
+from typing import List, Union
 import numpy as np
-import matplotlib
-from matplotlib.cm import ScalarMappable, get_cmap
-from matplotlib.colors import Normalize, ListedColormap
+import matplotlib.cm as mpl_cm
+import matplotlib.colors as mpl_colors
 from rdkit import Chem
 from rdkit.Chem	import rdDepictor
 from rdkit.Chem.Draw import	rdMolDraw2D
@@ -12,60 +11,62 @@ from IDLPPBopt.utils import extract_privileged_substructures
 
 class AtomAttentionVisualizer:
 
-    # _default_cmap_colors = get_cmap("autumn_r", 256)(np.linspace(0, 1, 256))
+    # _default_cmap_colors = cmget_cmap("autumn_r", 256)(np.linspace(0, 1, 256))
     # _default_cmap_colors[:192,3] = np.logspace(-4, 0, 192)
     # DEFAULT_CMAP = ListedColormap(_default_cmap_colors)
     DEFAULT_CMAP = "YlOrRd"
 
-    def __init__(self, smiles: str, atom_weights: np.ndarray, substructures: List[int]=None, *, 
-                 cmap=None, vmin=None, vmax=None, bond_highlight_color=None, 
-                 figsize: List[int]=None):
-        self.smiles = smiles
-        self.weights = atom_weights
-        self.substructures = substructures or []
+    def __init__(self, figsize: List[int]=None, cmap=None, vmin=None, vmax=None, substructure_colors=None):
+        self.figsize = figsize or (400, 300)
         self.cmap = cmap or self.DEFAULT_CMAP
         self.vmin = vmin
         self.vmax = vmax
-        self.bond_highlight_color = bond_highlight_color or (.9, .5, 0., 1.)
-        self.figsize = figsize or (400, 300)
+        self.substructure_colors = (substructure_colors or [mpl_colors.to_rgba(c) for c in mpl_cm.Set2.colors])
 
-    def highlight_privileged_substructures(self):
-        self.substructures.extend(
-            extract_privileged_substructures(self.smiles, self.weights))
-
-
-    def draw_svg(self):
+    def draw_svg(self, 
+                 smiles: str, 
+                 atom_weights: np.ndarray, 
+                 substructures: Union[str,List[int]]='privileged',
+                 *,
+                 cmap=None,
+                 substructure_colors=None):
         '''Return visualization of mol as SMILES'''
-        # Prepare molecule for 2d depiction
-        mol = Chem.MolFromSmiles(self.smiles)
-        rdDepictor.Compute2DCoords(mol)
+        # Prepare default values
+        if cmap is None:
+            cmap = self.cmap
+        if substructure_colors is None:
+            substructure_colors = self.substructure_colors
+        if substructures is None:
+            substructures = []
+        elif isinstance(substructures, str) and substructures == "privileged":
+            substructures = extract_privileged_substructures(smiles, atom_weights)
 
         # Define atom highlight colors
-        norm = Normalize(vmin=(self.vmin or self.weights.min()), 
-                         vmax=(self.vmax or self.weights.max()))
-        colo = ScalarMappable(norm=norm,cmap=self.cmap)
-        
-        atom_colors = {i: colo.to_rgba(wgt) for i,wgt in enumerate(self.weights)}
+        norm = mpl_colors.Normalize(vmin=(self.vmin or np.min(atom_weights)), vmax=(self.vmax or np.max(atom_weights)*1.3))
+        cmap = mpl_cm.ScalarMappable(norm=norm, cmap=cmap)
+        atom_colors = {i: cmap.to_rgba(wgt) for i,wgt in enumerate(atom_weights)}
         atom_indices = sorted(atom_colors.keys())
-        
-        # Define bond highlight colors
+
+        # Prepare substructures, define highlight colors
+        mol = Chem.MolFromSmiles(smiles)
         bond_indices, bond_colors = [], {}
         for bond in mol.GetBonds():
-            i,j = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
-            for subs in self.substructures:
-                if i in subs and j in subs:
-                    bndidx = bond.GetIdx()
-                    bond_indices.append(bndidx)
-                    bond_colors[bndidx] = self.bond_highlight_color
+            bi, bj = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+            for i, subs in enumerate(substructures):
+                if bi in subs and bj in subs:
+                    bond_index = bond.GetIdx()
+                    bond_indices.append(bond_index)
+                    bond_colors[bond_index] = substructure_colors[i % len(substructure_colors)]
                     break
-
+        
+        rdDepictor.Compute2DCoords(mol)
         drawer = rdMolDraw2D.MolDraw2DSVG(self.figsize[0], self.figsize[1])
         drawer.SetFontSize(.7)
         drawer.DrawMolecule(
             rdMolDraw2D.PrepareMolForDrawing(mol), 
-            highlightAtoms=range(len(self.weights)),
+            highlightAtoms=atom_indices, 
             highlightAtomColors=atom_colors,
-            highlightBonds=bond_indices,          
+            highlightBonds=bond_indices, 
             highlightBondColors=bond_colors)
         drawer.FinishDrawing()
         svg = drawer.GetDrawingText()
